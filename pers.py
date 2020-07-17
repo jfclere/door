d54c86fb3e1ffaa5965143e596b2b60a033cca59
root <root@localhost.localdomain>
2020-07-17 07:42:16 -0400

    FIrst version that makes sense ;-)

--- pers.py
+++ pers.py
@@ -4,14 +4,15 @@ import time
 
 # relays
 out1 = 21
-out2 = 21
+out2 = 20
 
-# switch
+# switch input (zero when pushed)
 up = 26
 down = 16
 
 # Pin Setup:
 GPIO.setwarnings(False)
+GPIO.cleanup()
 GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
 GPIO.setup(up, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Switch up
 GPIO.setup(down, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Switch down
@@ -19,8 +20,22 @@ GPIO.setup(down, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Switch down
 GPIO.setup(out1, GPIO.OUT) # Relay 1 pin set as outout
 GPIO.setup(out2, GPIO.OUT) # Relay 2 pin set as output
 
+# When press go to zero (should be 1).
 while True:
+    action = False
+    time.sleep(0.1)
     rup = GPIO.input(up)
     rdown = GPIO.input(down)
-    time.sleep(1)
-    print("up : ", rup, " down: ", rdown);
+    # default to up
+    if rup == 0:
+       # up is pressed Switch up and action.
+       GPIO.output(out1, GPIO.HIGH)
+       GPIO.output(out2, GPIO.LOW) 
+       action = True
+    if rdown == 0:
+       # down is pressed Switch down and action.
+       GPIO.output(out1, GPIO.LOW)
+       GPIO.output(out2, GPIO.LOW) 
+       action = True
+    if action == False:
+       GPIO.output(out2, GPIO.HIGH) 
