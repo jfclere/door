84f2ee4f233ecf5eb3190735d5a6ccef8293061b
root <root@localhost.localdomain>
2020-07-27 13:13:25 +0200

    look for wrong and exist users.

--- blockbadguys.sh
+++ blockbadguys.sh
@@ -1,6 +1,8 @@
-#journalctl -xe | grep "Invalid user" | awk '{ print $10 }' | sort -u | awk '{ print "firewall-cmd --permanent --zone=FedoraServer  --add-rich-rule=\"rule family='ipv4' source address='" $0 "' reject\"" }'
-journalctl -xe | grep "Invalid user" | awk '{ print $10 }' | sort -u > /tmp/$$.tmp
+journalctl -xe | grep "Failed password for" > /tmp/$$.tmp.1
+cat /tmp/$$.tmp.1 | grep "invalid user" | awk '{ print $13 }' > /tmp/$$.tmp
+cat /tmp/$$.tmp.1 | grep -v "invalid user" | awk '{ print $11 }' >> /tmp/$$.tmp
+sort -u /tmp/$$.tmp > /tmp/$$.tmp.2
 while read a
 do
   echo "firewall-cmd --permanent --zone=FedoraServer  --add-rich-rule=\"rule family='ipv4' source address='"$a"' reject\""
-done < /tmp/$$.tmp
+done < /tmp/$$.tmp.2
