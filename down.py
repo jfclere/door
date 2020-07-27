1e281da78581e886c023e445ae19042eb53e5142
root <root@localhost.localdomain>
2020-07-27 09:15:28 +0200

    Add up/down and short up to get light.

---
+++ down.py
@@ -0,0 +1,26 @@
+# External module imports
+import RPi.GPIO as GPIO
+import time
+
+# relays
+out1 = 20
+out2 = 21
+
+# switch input (zero when pushed)
+up = 26
+down = 16
+
+# Pin Setup:
+GPIO.setwarnings(False)
+GPIO.cleanup()
+GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+GPIO.setup(up, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Switch up
+GPIO.setup(down, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Switch down
+
+GPIO.setup(out1, GPIO.OUT) # Relay 1 pin set as outout
+GPIO.setup(out2, GPIO.OUT) # Relay 2 pin set as output
+
+print("DOWN")
+GPIO.output(out1, GPIO.HIGH)
+GPIO.output(out2, GPIO.LOW) 
+time.sleep(10)
---
+++ light.py
@@ -0,0 +1,28 @@
+# External module imports
+import RPi.GPIO as GPIO
+import time
+
+# relays
+out1 = 20
+out2 = 21
+
+# switch input (zero when pushed)
+up = 26
+down = 16
+
+# Pin Setup:
+GPIO.setwarnings(False)
+GPIO.cleanup()
+GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+GPIO.setup(up, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Switch up
+GPIO.setup(down, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Switch down
+
+GPIO.setup(out1, GPIO.OUT) # Relay 1 pin set as outout
+GPIO.setup(out2, GPIO.OUT) # Relay 2 pin set as output
+
+print("UP")
+GPIO.output(out1, GPIO.HIGH)
+GPIO.output(out2, GPIO.HIGH) 
+time.sleep(0.5)
+print("OFF")
+GPIO.output(out1, GPIO.LOW)
